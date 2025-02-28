c67d4d31bf383359508499dd67d1a223d8fe4944
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-28 22:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ const btn = document.getElementById('btn');
 btn.addEventListener('click', ()=>{
 	fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9bac5b0780f828c74f54b5c028be6aba")
 	.then(response => response.json())
-	.then(data => console.log(data))
-	.catch(error => console.error('Error:', error));
+	.then(data => {
 
 	const displayField = document.createElement('div');
 	displayField.id = "weatherData";
-	displayField.innerHTML = `Current weather in London: Clouds`
-});
+	displayField.innerHTML = `Current weather in London: ${data.weather[0].main}`;
+	document.body.appendChild(displayField);
+})
+.catch(err => console.error('Error:', err));
+}); 
