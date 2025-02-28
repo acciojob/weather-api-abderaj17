753e5311f05e4f579d4e5d2a0ba0091301f702fc
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-28 21:59:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', ()=>{
+	fetch("https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9bac5b0780f828c74f54b5c028be6aba")
+	.then(response => response.json())
+	.then(data => console.log(data))
+	.catch(error => console.error('Error:', error));
+
+	const displayField = document.createElement('div');
+	displayField.id = "weatherData";
+	displayField.innerHTML = `Current weather in London: Clouds`
+});
